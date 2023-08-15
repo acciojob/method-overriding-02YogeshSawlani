9839fb25ca1f0840dec37b7032ef4450767b6326
yoges <yogeshsawlanisawlani143@gmail.com>
2023-08-15 20:28:01 +0530

    override

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,12 +4,15 @@ public class Main {
     public static class A {
         public String meth()
         {
+
             return "Invoking method from class A";
         }
     }
     public static class B extends A{
+        @Override
         public String meth()
         {
+
             return "Method is overridden in Extended class B";
         }
     }
