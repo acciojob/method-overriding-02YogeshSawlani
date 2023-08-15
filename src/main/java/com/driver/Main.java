813994cc74b5b0d15af34b33f8b571908f193f8d
yoges <yogeshsawlanisawlani143@gmail.com>
2023-08-15 17:32:49 +0530

    Method Override

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,9 +1,23 @@
 package com.driver;
 
 public class Main {
+    public static class A {
+        public static String meth()
+        {
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A{
+        public static String meth()
+        {
+            return "Method is overridden in Extended class B";
+        }
+    }
   public static void main(String[]args)
   {
       B obj=new B();
-      System.out.println(obj.meth());
+      obj.meth();
+      B obj1=new B();
+      obj1.meth();
   }
 }
